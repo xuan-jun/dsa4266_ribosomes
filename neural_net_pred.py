fb3a713ca3b0f27b617a7d4817c67456512ffd56
xuan-jun <xuanjun1999@gmail.com>
2023-10-22 01:56:37 +0800

    added argparse for neural net training and prediction

--- neural_net_pred.py
+++ neural_net_pred.py
@@ -1,76 +1,113 @@
-# libraries for processing
+# general purpose libraries
 import os
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import argparse
 
 # libraries for neural networks
 import torch
 import torch.nn as nn
 
-# importing Neural Network Model
+# model libraries
 from neural_net_model import NeuralNetModel
 from neural_net_pre_process import RNAData
 
 # CONSTANTS
-MODEL_STATE_DICT = os.path.join(".", "state", "model.pth") 
+def parse_arguments():
+    """Parses the arguments that are supplied by the user
+
+    Returns:
+        Namespace: parsed arguments from the user
+    """
+    parser = argparse.ArgumentParser(description="Runs the prediction for the trained Neural Network of m6a modifications")
+    required = parser.add_argument_group("required arguments")
+    required.add_argument("-msd", "--modelstate-dict", type=str, metavar="",
+                        required=True, help="Full filepath to where the model state dict is stored during the Neural Network training")
+    required.add_argument("-dp", "--data-path", type=str, metavar="", 
+                        required=True, help="Full filepath to where the .json.gz file of the data that we want to predict is located")
+    optional = parser.add_argument_group("optional arguments")
+    optional.add_argument("-rf", "--results-folder", type=str, default=os.path.join(".", "results"), metavar="",
+                        help="Full path to the directory where the result of the prediction should be stored. (Default: ./results/)")
+    optional.add_argument("-bts", "--batch-size", type=int, default=64, metavar="",
+                        help="Batch size that was used for training the Neural Network (Default: 64)")
+    optional.add_argument("-rs", "--read-size", type=int, default=20 , metavar="", 
+                        help="Read size that was used for training the Neural Network (Default: 20)")
+    args = parser.parse_args()
+    return args
+
+MODEL_STATE_DICT = os.path.join(".", "state", "model_param.pth") 
 DATA_FOLDER_PATH = os.path.join(".", "data") 
 RESULTS_FOLDER_PATH = os.path.join(".", "results")
-DATA_FILE_NAME = "dataset0.json.gz"
-BATCH_SIZE = 128 # note that this needs to be the same the training process
+DATA_FILE_NAME = "dataset2.json.gz"
+BATCH_SIZE = 64 # note that this needs to be the same the training process
 READ_SIZE = 20 # note that this needs to be the same the training process
 
-if not (os.path.exists(MODEL_STATE_DICT)):
-    raise Exception("Model state does not exist, please check the path or if training has been done!")
+def predict_neural_network(args):
+    """Runs predictions on a trained NeuralNetModel based on the arguments that are passed in by the user
 
-# initialising the model and dataloader
-rna_data = RNAData(data_path=os.path.join(DATA_FOLDER_PATH, DATA_FILE_NAME),
-                   read_size=READ_SIZE, batch_size=BATCH_SIZE, train=False)
-rna_dataloader = rna_data.data_loader()
+    Args:
+        args (Namespace): contains the arguments that are passed in by the user through argparser
+    """
 
-model = NeuralNetModel(batch_size=BATCH_SIZE, read_size=READ_SIZE)
-model.load_state_dict(torch.load(MODEL_STATE_DICT))
-model.eval() # set to evaluation mode
+    # check if the path model state exists
+    if not (os.path.exists(args.modelstate_dict)):
+        raise Exception("Model state does not exist, please check the path or if training has been done!")
 
-# create the results directory
-if not (os.path.exists(RESULTS_FOLDER_PATH)):
-    os.mkdir(RESULTS_FOLDER_PATH)
+    # initialising the model and dataloader
+    rna_data = RNAData(data_path=args.data_path, read_size=args.read_size,
+                       batch_size=args.batch_size, train=False)
+    rna_dataloader = rna_data.data_loader()
 
-# creating the results file name based on the original data value 
-results_file_name = f"{DATA_FILE_NAME.split('.')[0]}.csv"
+    # initialising the neural net model
+    model = NeuralNetModel(batch_size=args.batch_size, read_size=args.read_size)
+    model.load_state_dict(torch.load(args.modelstate_dict))
+    model.eval() # set to evaluation mode
 
-## running the test 
+    # create the results directory if it doesnt exist
+    if not (os.path.exists(args.results_folder)):
+        os.mkdir(args.results_folder)
 
-# stores the predicted_scores for each evaluation data
-pred_scores = torch.tensor([])
-site_indices = []
+    # creating the results file name based on the original data value 
+    results_file_name = f"{os.path.basename(args.data_path).split('.')[0]}.csv"
 
-# run without computing gradients
-print("Predicting now...")
-with torch.no_grad():
-    
-    # loop through testing data
-    for features, index in tqdm(rna_dataloader, total=len(rna_dataloader)):
+    ## running the test 
 
-        index = index.flatten().numpy()
-        # keep track of the indices
-        site_indices.extend(index)
+    # stores the predicted_scores for each evaluation data
+    pred_scores = torch.tensor([])
+    site_indices = []
 
-        # use the model to predict the values
-        y_pred = model(features)
+    # run without computing gradients
+    print("Predicting now...")
+    with torch.no_grad():
+        
+        # loop through testing data
+        for features, index in tqdm(rna_dataloader, total=len(rna_dataloader)):
 
-        # combining the previous scores and the current scores
-        pred_scores = torch.concat([pred_scores, y_pred])
-    
-print("Preparing output file...")
-# tidying up the data and converting into csv
-pred_scores = pred_scores.numpy().flatten().astype(float)
+            index = index.flatten().numpy()
+            # keep track of the indices
+            site_indices.extend(index)
 
-rna_test_sites = rna_data.train_transcript_position
-transcript_id = list(map(lambda x : rna_test_sites[x][0], site_indices))
-transcript_position = list(map(lambda x : rna_test_sites[x][1], site_indices))
+            # use the model to predict the values
+            y_pred = model(features)
 
-final_data = pd.DataFrame({"transcript_id" : transcript_id,
-                           "transcript_position" : transcript_position,
-                           "score" : pred_scores})
-final_data.to_csv(os.path.join(RESULTS_FOLDER_PATH, results_file_name), index=False)
+            # combining the previous scores and the current scores
+            pred_scores = torch.concat([pred_scores, y_pred])
+        
+    print("Preparing output file...")
+    # tidying up the data and converting into csv
+    pred_scores = pred_scores.numpy().flatten().astype(float)
+
+    rna_test_sites = rna_data.train_transcript_position
+    transcript_id = list(map(lambda x : rna_test_sites[x][0], site_indices))
+    transcript_position = list(map(lambda x : rna_test_sites[x][1], site_indices))
+
+    final_data = pd.DataFrame({"transcript_id" : transcript_id,
+                            "transcript_position" : transcript_position,
+                            "score" : pred_scores})
+    final_data.to_csv(os.path.join(args.results_folder, results_file_name), index=False)
+    print("Output file saved!")
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    predict_neural_network(args)
--- neural_net_training.py
+++ neural_net_training.py
@@ -2,6 +2,7 @@
 import numpy as np
 import os
 from tqdm import tqdm
+import argparse
 
 # torch libraries
 import torch
@@ -13,88 +14,134 @@ from torcheval.metrics.functional import binary_auprc
 from neural_net_model import NeuralNetModel
 from neural_net_pre_process import RNAData
 
-# CONSTANTS
-DATA_PATH = os.path.join(".", "data", "dataset0.json.gz")
-LABEL_PATH = os.path.join(".", "data", "data.info")
-MODEL_STATE_DIR = os.path.join(".", "state")
-MODEL_STATE_FILENAME = "model_param.pth"
-LEARNING_RATE = 0.001
-BATCH_SIZE = 128
-READ_SIZE = 20
-NUM_EPOCHS = 11 
+def parse_arguments():
+    """Parses the arguments that are supplied by the user
 
-# preparing the data
-rna_data = RNAData(data_path=DATA_PATH, label_path=LABEL_PATH, batch_size=BATCH_SIZE, read_size=READ_SIZE)
-print(f"Length of training data: {len(rna_data)}")
-rna_dataloader = rna_data.data_loader()
+    Returns:
+        Namespace: parsed arguments from the user
+    """
+    parser = argparse.ArgumentParser(description="Trains the Neural Network for predicting m6a modifications")
+    required = parser.add_argument_group("required arguments")
+    required.add_argument('-dp', '--data-path',
+                        metavar='', type=str, required=True,help="Full path to the .json.gz file containing the data for training")
+    required.add_argument('-lp', '--label-path',
+                        metavar='', type=str, required=True, help="Full path to the .info file containing the labels of the data used for training")
+    optional = parser.add_argument_group("optional arguments")
+    optional.add_argument('-msd', '--modelstate-dict',
+                        metavar='', type=str, default=os.path.join(".", "state", "model.pth"),
+                        help="Full filepath to where we want to store the model state (Default: ./state/model.pth)")
+    optional.add_argument('-lr', '--learning-rate',
+                        metavar='', type=float, default=0.001, help="Learning rate for the Neural Network (Default:0.001)")
+    optional.add_argument('-bts', '--batch-size',
+                        metavar='', type=int, default=64, help="Batch size for the Neural Network (Default: 64)")
+    optional.add_argument('-rs', '--read-size',
+                        metavar='', type=int, default=20,
+                        help="Number of reads that is used per site for prediction by the Neural Network (Default: 20)")
+    optional.add_argument('-ep', '--num-epochs',
+                        metavar='', type=int, default=30, help="Number of epochs for training the Neural Network (Default: 30)")
+    args = parser.parse_args()
+    return args
 
-# initalizing the model
-model = NeuralNetModel(batch_size=BATCH_SIZE, read_size=READ_SIZE)
+def evaluate_neural_net(model, rna_data):
+    """Evaluates the performance of the Neural Network based on the Area under ROC and PRC
 
-# binary cross entropy loss function
-criterion = nn.BCELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    Args:
+        model (NeuralNetModel): neural network model that was used for training
+        rna_data (RNAData): RNAData object that was created during the training process
 
-# check if the state dictionary exists, if it doesnt, create it
-if not (os.path.exists(MODEL_STATE_DIR)):
-    os.mkdir(MODEL_STATE_DIR)
+    Returns:
+        tuple: tuple pair with the area under ROC curve and area under PR curve
+    """
+    # turn the model to evaluation mode
+    model.eval()
 
-print("Starting Training...")
-for epoch in range(NUM_EPOCHS):
-    # set to training mode
-    model.train()
-    loop = tqdm(enumerate(rna_dataloader),
-                total=len(rna_dataloader))
-    for i, (x, y) in loop:
-        label = y.flatten().type(torch.float32)
+    # stores the predicted_scores and actual labels for each evaluation data
+    pred_scores = torch.tensor([])
+    lab = torch.tensor([]).type(torch.float32)
+
+    print("Starting Evaluation...")
+    # run without computing gradients
+    with torch.no_grad():
         
-        # forward pass
-        outputs = model(x)
-        loss = criterion(outputs, label)
+        # get the evaluation data and data loader
+        rna_data.eval_mode()
+        eval_rna_dataloader = rna_data.data_loader()
 
-        # backward pass
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+        # loop through eval data
+        for features, labels in eval_rna_dataloader:
 
-        loop.set_description(f"Epoch [{epoch+1}/{NUM_EPOCHS}]")
-        loop.set_postfix(loss=loss.item())
+            labels = labels.flatten().type(torch.float32)
+            # keep track of all the labels
+            lab = torch.cat([lab, labels])
 
-        # saving the model parameters
-        torch.save(model.state_dict(), os.path.join(MODEL_STATE_DIR, MODEL_STATE_FILENAME))
+            # use the model to predict the values
+            y_pred = model(features)
 
-# evaluation
+            # combining the previous scores and the current scores
+            pred_scores = torch.cat([pred_scores, y_pred])
 
-model = NeuralNetModel(batch_size=BATCH_SIZE, read_size=READ_SIZE)
-model.load_state_dict(torch.load(os.path.join(MODEL_STATE_DIR, MODEL_STATE_FILENAME)))
-model.eval()
+    # evaluating the ROC and PR AUC Scores
+    auroc = torchmetrics.AUROC(task="binary")
+    roc_score = auroc(pred_scores, lab).item()
+    pr_score = binary_auprc(pred_scores, lab).item()
+    print(f"AU ROC Score: {roc_score:.4f}")
+    print(f"AU PRC Score : {pr_score:.4f}")
 
-# stores the predicted_scores and actual labels for each evaluation data
-pred_scores = torch.tensor([])
-lab = torch.tensor([]).type(torch.float32)
+    return (roc_score, pr_score)
 
-print("Starting Evaluation...")
-# run without computing gradients
-with torch.no_grad():
-    
-    # get the evaluation data and data loader
-    rna_data.eval_mode()
-    eval_rna_dataloader = rna_data.data_loader()
 
-    # loop through eval data
-    for features, labels in eval_rna_dataloader:
+def train_neural_net(args):
+    """Trains a NeuralNetModel based on the arguments that are passed in by the user
 
-        labels = labels.flatten().type(torch.float32)
-        # keep track of all the labels
-        lab = torch.cat([lab, labels])
+    Args:
+        args (Namespace): contains the arguments that are passed in by the user through argparser
+    """
 
-        # use the model to predict the values
-        y_pred = model(features)
+    # preparing the data
+    rna_data = RNAData(data_path=args.data_path, label_path=args.label_path,
+                       batch_size=args.batch_size, read_size=args.read_size)
+    print(f"Length of training data: {len(rna_data)}")
+    rna_dataloader = rna_data.data_loader()
 
-        # combining the previous scores and the current scores
-        pred_scores = torch.cat([pred_scores, y_pred])
+    # initalizing the model
+    model = NeuralNetModel(batch_size=args.batch_size, read_size=args.read_size)
 
-# evaluating the ROC and PR AUC Scores
-auroc = torchmetrics.AUROC(task="binary")
-print(f"AU ROC Score: {auroc(pred_scores, lab).item():.4f}")
-print(f"AU PRC Score : {binary_auprc(pred_scores, lab).item():.4f}")
+    # binary cross entropy loss function
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+
+    # check if the state dictionary exists, if it doesnt, create it
+    if not (os.path.exists(os.path.dirname(args.modelstate_dict))):
+        os.mkdir(os.path.dirname(args.modelstate_dict))
+
+    print("Starting Training...")
+    for epoch in range(args.num_epochs):
+        # set to training mode
+        model.train()
+        loop = tqdm(enumerate(rna_dataloader),
+                    total=len(rna_dataloader))
+        for i, (x, y) in loop:
+            label = y.flatten().type(torch.float32)
+            
+            # forward pass
+            outputs = model(x)
+            loss = criterion(outputs, label)
+
+            # backward pass
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            loop.set_description(f"Epoch [{epoch+1}/{args.num_epochs}]")
+            loop.set_postfix(loss=loss.item())
+
+            # saving the model parameters
+            torch.save(model.state_dict(), args.modelstate_dict)
+
+    # evaluation
+    evaluate_neural_net(model, rna_data)
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    train_neural_net(args)
